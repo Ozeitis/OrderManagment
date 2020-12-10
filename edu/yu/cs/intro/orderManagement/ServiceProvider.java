6407063125f4a827e264a36c7d11d08937d7058c
sjs221 <75805965+sjs221@users.noreply.github.com>
2020-12-10 15:33:03 -0500

    Update ServiceProvider.java

--- edu/yu/cs/intro/orderManagement/ServiceProvider.java
+++ edu/yu/cs/intro/orderManagement/ServiceProvider.java
@@ -1,73 +1,81 @@
 package edu.yu.cs.intro.orderManagement;
 import java.util.Set;
 import java.util.HashSet;
+
 public class ServiceProvider{
 	private String name;
 	private int id;
 	private Set<Service> services;
 	private boolean busy = false;
 	//this is to mark whether the service provider is busy or not. it is modified by other methods.
+	
 	public ServiceProvider(String name, int id, Set<Service> services){
-this.name=name;
-this.id=id;
-this.services = new HashSet<>;
-this.services.addAll(services);
-}
-public String getName(){
-	return this.name;
-}
-public int getId(){
-	return this.id;
-}
-protected void assignToCustomer(){
-	if(this.busy==true){
-		throw IllegalStateException;
-	}else{
-		this.busy=true;
+		this.name=name;
+		this.id=id;
+		this.services = new HashSet<>;
+		this.services.addAll(services);
 	}
-
-}
-protected void endCustomerEngagement(){
-	if(this.busy==false){
-		throw IllegalStateException;
-	}else{
-		this.busy=false;
-	}
-}
-protected boolean addService(Service s){
-	return this.services.add(s);
-}
-protected boolean removeService(Service s){
-	return this.services.remove(s);
-}
-public Set<Service> getServices(){
-	Set<Service> copy = new Set<>;
-	copy.addAll(this.services);
-	return copy;
-}
-@Override
-public boolean equals(Object o){
-	if(this=o){
-		return true;
-	}
-	if( o== null){
-		return false;
-	}
-	if(getClass() !=o.getClass()){
-		return false;
-	}
-	if(this.getId = o.getId){
-		return true;
-	}
-	else return false;
 	
+	public String getName(){
+		return this.name;
+	}
 	
-}
-@Override
-public int hashCode(){
-	int cipher = this.id * 54;
-	for(int a = 0; a <)
-return 
-}
+	public int getId(){
+		return this.id;
+	}
+	
+	protected void assignToCustomer(){
+		if(this.busy==true){
+			throw IllegalStateException;
+		}else{
+			this.busy=true;
+		}
+	}
+	
+	protected void endCustomerEngagement(){
+		if(this.busy==false){
+			throw IllegalStateException;
+		}else{
+			this.busy=false;
+		}
+	}
+	
+	protected boolean addService(Service s){
+		return this.services.add(s);
+	}
+	
+	protected boolean removeService(Service s){
+		return this.services.remove(s);
+	}
+	
+	public Set<Service> getServices(){
+		Set<Service> copy = new Set<>;
+		copy.addAll(this.services);
+		return copy;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(this = o){
+			return true;
+		}
+		if(o == null){
+			return false;
+		}
+		if(getClass() !=o.getClass()){ //You can probably compine the tests which return false
+			return false;
+		}
+		if(this.getId = o.getId){
+			return true;
+		}
+		else return false; //you can just return this.getId = o.getId and cut down the lines of code
+	}
+	
+	@Override
+	public int hashCode(){
+		int cipher = this.id * 54;
+		for(int a = 0; a <)
+	return 
+	}
 
 }
