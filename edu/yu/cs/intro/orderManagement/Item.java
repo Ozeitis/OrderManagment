f59ad140ab886739eb9932f6fcc219856d4fbc26
Oze <oze@obotach.com>
2020-12-10 09:52:50 -0800

    UPDATED TO LATEST INSTRUCTIONS BY JUDAH

--- edu/yu/cs/intro/orderManagement/Item.java
+++ edu/yu/cs/intro/orderManagement/Item.java
@@ -1,7 +1,9 @@
 package edu.yu.cs.intro.orderManagement;
 
 public interface Item {
-int getItemNumber();
-String getDescription();
-double getPrice();
+    int getItemNumber();
+
+    String getDescription();
+
+    double getPrice();
 }
--- edu/yu/cs/intro/orderManagement/Order.java
+++ edu/yu/cs/intro/orderManagement/Order.java
@@ -1,39 +1,64 @@
 package edu.yu.cs.intro.orderManagement;
+
 /**
- * models an order placed by a customer. An item in the order can be an instance of either Product
-or Service */
-6
+ * models an order placed by a customer. An item in the order can be an instance
+ * of either Product or Service
+ */
 public class Order {
-public Order(){}
-/**
-* @return all the items (products and services) in the order */
-public Item[] getItems(){}
-/**
-* @param b
-* @return the quantity of the given item ordered in this order. Zero if the item is not in the
-order. */
-public int getQuantity(Item b){ }
-/**
-* Add the given quantity of the given item (product or service) to the order * @param item
-* @param quantity
-*/
-public void addToOrder(Item item, int quantity){}
+    public Order() {
+    }
+
     /**
-     * Calculate the total price of PRODUCTS in the order. Must multiply each item's price by the
-quantity.
-* @return the total price of products in this order */
-public double getProductsTotalPrice(){}
+     * @return all the items (products and services) in the order
+     */
+    public Item[] getItems() {
+    }
+
     /**
-     * Calculate the total price of the SERVICES in the order. Must multiply each item's price by
-the quantity.
-* @return the total price of products in this order */
-public double getServicesTotalPrice(){}
-/**
-* @return has the order been completed by the order management system? */
-public boolean isCompleted() {}
+     * @param b
+     * @return the quantity of the given item ordered in this order. Zero if the
+     *         item is not in the order.
+     */
+    public int getQuantity(Item b) {
+    }
+
+    /**
+     * Add the given quantity of the given item (product or service) to the order
+     * * @param item
+     * 
+     * @param quantity
+     */
+    public void addToOrder(Item item, int quantity) {
+    }
+
+    /**
+     * Calculate the total price of PRODUCTS in the order. Must multiply each item's
+     * price by the quantity.
+     * 
+     * @return the total price of products in this order
+     */
+    public double getProductsTotalPrice() {
+    }
+
+    /**
+     * Calculate the total price of the SERVICES in the order. Must multiply each
+     * item's price by the quantity.
+     * 
+     * @return the total price of products in this order
+     */
+    public double getServicesTotalPrice() {
+    }
+
+    /**
+     * @return has the order been completed by the order management system?
+     */
+    public boolean isCompleted() {
+    }
+
     /**
      * Indicate if the order has been completed by the order management system
-* @param completed
-*/
-public void setCompleted(boolean completed) {}
+     * * @param completed
+     */
+    public void setCompleted(boolean completed) {
+    }
 }
