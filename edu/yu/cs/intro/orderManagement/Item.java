99e50130f5640162add76c65273851a09bd0747b
TGfor3 <75807593+TGfor3@users.noreply.github.com>
2020-12-17 17:50:03 -0500

    Update Item.java

--- edu/yu/cs/intro/orderManagement/Item.java
+++ edu/yu/cs/intro/orderManagement/Item.java
@@ -1,10 +1,6 @@
 package edu.yu.cs.intro.orderManagement;
 public interface Item {
-    Object getItemNumber = null;
-
 	int getItemNumber();
-
-    String getDescription();
-
-    double getPrice();
+	String getDescription();
+	double getPrice();
 }
